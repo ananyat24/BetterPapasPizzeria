d4d979a152d123b1667d06a6a875fb38d6d37db4
JoshiRiddhi <s-ridjoshi@lwsd.org>
2024-03-12 13:16:37 -0700

    initial commit-working on sprites

---
+++ Player.py
@@ -0,0 +1,9 @@
+import pygame
+import os
+import sys
+import time
+import random
+
+class Player:
+    def __init__(self, username: str, sprite_sheet:str, start_x, start_y):
+        self.spritesheet = pygame.image.load(os.path.join())
