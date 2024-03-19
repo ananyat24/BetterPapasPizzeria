59dfdb92c0fc8634cf552d91f4b4c5488cbbd026
mmittal2 <mittal.medha@gmail.com>
2024-03-19 12:43:33 -0700

    Able to cut pizza by dragging across screen. #8

---
+++ adding_toppings.py
@@ -0,0 +1,20 @@
+import pygame
+from chef import Chef
+from constants import Constants
+from topping import Topping
+
+class AddingToppings(Chef):
+    
+    def __init__(self, screen:pygame.display):
+        self.c = Constants() # get the constants        
+        
+        toppings_board_img = pygame.image.load("ADD HERE")
+        toppings_board_image = pygame.transform.scale(toppings_board_img, (self.c.screen_width, self.c.screen_height))
+        
+        super().__init__(screen, toppings_board_image)
+        
+        # create each topping as a separate object
+        olives = Topping() # ADD ALL THE PARAMETERS HERE
+        self.topping_boxes.append(olives.get_image_location())
+    
+    # add a topping
