1a356b9c65c5df3f75d759ccd78bcaab16180b83
mmittal2 <mittal.medha@gmail.com>
2024-04-11 13:11:12 -0700

    Started working on chef adding toppings. #16

--- chef_files/adding_toppings.py
+++ chef_files/adding_toppings.py
@@ -5,16 +5,38 @@ from chef_files.topping import Topping
 
 class AddingToppings(Chef):
     
-    def __init__(self, screen:pygame.display):
-        self.c = Constants() # get the constants        
-        
-        toppings_board_img = pygame.image.load("ADD HERE")
-        toppings_board_image = pygame.transform.scale(toppings_board_img, (self.c.screen_width, self.c.screen_height))
-        
-        super().__init__(screen, toppings_board_image)
+    def __init__(self, screen:pygame.display, c:Constants):
+        self.c = c
+        self.pizza_image_location = (295, 65)
+        self.background_image = None
+        super().__init__(screen, self.c)
         
         # create each topping as a separate object
-        olives = Topping() # ADD ALL THE PARAMETERS HERE
-        self.topping_boxes.append(olives.get_image_location())
+        #artichoke = Topping("pictures/single_ingredients/artichoke.png", location:tuple, width:int, height:int, screen)
+        #self.topping_boxes.append(artichoke.one_topping_image, artichoke.location)
+        """
+        ^ steps needed:
+        1. create each topping as a separate object
+        2. change all location, width, and height parameters to their proper values
+        3. figure out how to get the topping image to follow the cursor
+        4. using colliderect and mouse clicks, determine when to pick up and drop toppings
+        5. add a done button that allows user to move to next stage (technically baking the pizza but cutting the pizza for now) - there might be a button on the screen already just need to add the code
+        """
+
+        
+    # switch the background to the toppings board
+    def bg_to_toppings_board(self):
+        self.background_image = self.change_background("background images/kitchen backgrounds/Kitchen - Ingredients BG.png")
+        
     
-    # add a topping
+    # have the pizza fly in initially
+    def pizza_fly_in(self):
+        self.c.pizza_image_location = self.pizza_image_location
+        final_y_coor = self.c.pizza_image_location[1]
+        for y_coor in range(0, final_y_coor, 20):
+            self.screen.blit(self.c.background_image, (0, 0))
+            self.screen.blit(self.c.pizza_image, (self.c.pizza_image_location[0], y_coor))
+            pygame.display.update()
+        self.screen.blit(self.c.background_image, (0, 0))
+        self.screen.blit(self.c.pizza_image, (self.c.pizza_image_location[0], final_y_coor))
+        
--- constants.py
+++ constants.py
@@ -1,5 +1,6 @@
 # store all constants used across multiple files here
 import socket
+# also store variables that are changed throughout code but needed in multiple files here
 
 class Constants():
     
@@ -9,4 +10,10 @@ class Constants():
         self.HOST_IP = socket.gethostbyname('localhost') #replace 'localhost' with socket.gethostname() for other laptops
         self.HOST_PORT = 12345
         self.ENCODER = 'utf-8'
+        self.background_image = None
+        
+        self.pizza_image = None # image for whole pizza
+        self.pizza_image_location = () # location for pizza in the form (x_pos, y_pos)
+        self.lines = [] # lines for cut pizza
+        self.topping_boxes = [] # list of tuples in the form (image, location) for all topping containers
         
