af33859ad17e0f0c32ffcd90e157d26778d5f654
Ananya T <trip.ananya@gmail.com>
2024-04-18 12:34:44 -0700

    send ticket values to library to share with another player. fix #27

--- constants.py
+++ constants.py
@@ -2,8 +2,10 @@
 import socket
 # also store variables that are changed throughout code but needed in multiple files here
 
+import os
+import json
+
 class Constants():
-    
     def __init__(self):
         self.screen_height = 765
         self.screen_width = 1350
@@ -16,4 +18,15 @@ class Constants():
         self.pizza_image_location = () # location for pizza in the form (x_pos, y_pos)
         self.lines = [] # lines for cut pizza
         self.topping_boxes = [] # list of tuples in the form (image, location) for all topping containers
-        
+
+        self.VALUES_JSON = {}
+        self.VALUES = {}
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "values.json")) as f:
+            self.VALUES_JSON = json.load(f)
+
+    def ticketLoad(self):
+        self.VALUES = self.VALUES_JSON.copy()
+
+    def ticketSave(self):
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "values.json"), "w") as f:
+            json.dump(self.VALUES_JSON, f)
