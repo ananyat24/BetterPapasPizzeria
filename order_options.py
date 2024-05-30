dd1bfd28db131047f89e131fe94a8e162f7d9f14
Ananya T <trip.ananya@gmail.com>
2024-05-30 12:44:38 -0700

    fixed all dimensions for update bg image #21

--- order_options.py
+++ order_options.py
@@ -51,33 +51,33 @@ class orderOptions:
         self.bg = pygame.transform.scale(self.bg, (self.WIDTH, self.HEIGHT))
         self.screen.blit(self.bg, (0, 0))
 
-        self.toppingArea1 = pygame.Rect(780, 160, 42, 42)
-        self.toppingArea2 = pygame.Rect(780, 200, 42, 42)
-        self.toppingArea3 = pygame.Rect(780, 240, 42, 42)
-        self.toppingArea4 = pygame.Rect(780, 280, 42, 42)
-        self.toppingArea5 = pygame.Rect(780, 320, 42, 42)
-        self.toppingArea6 = pygame.Rect(780, 360, 42, 42)
-        self.toppingArea7 = pygame.Rect(780, 400, 42, 42)
+        self.toppingArea1 = pygame.Rect(1054, 155, 65, 55)
+        self.toppingArea2 = pygame.Rect(1054, 206, 65, 55)
+        self.toppingArea3 = pygame.Rect(1054, 257, 65, 55)
+        self.toppingArea4 = pygame.Rect(1054, 308, 65, 55)
+        self.toppingArea5 = pygame.Rect(1054, 359, 65, 55)
+        self.toppingArea6 = pygame.Rect(1054, 410, 65, 55)
+        self.toppingArea7 = pygame.Rect(1054, 461, 65, 55)
 
-        self.multiplierArea1 = pygame.Rect(940, 165, 42, 42)
-        self.multiplierArea2 = pygame.Rect(940, 205, 42, 42)
-        self.multiplierArea3 = pygame.Rect(940, 245, 42, 42)
-        self.multiplierArea4 = pygame.Rect(940, 285, 42, 42)
-        self.multiplierArea5 = pygame.Rect(940, 325, 42, 42)
-        self.multiplierArea6 = pygame.Rect(940, 365, 42, 42)
-        self.multiplierArea7 = pygame.Rect(940, 405, 42, 42)
+        self.ingredientArea1 = pygame.Rect(1130, 160, 120, 50)
+        self.ingredientArea2 = pygame.Rect(1130, 211, 120, 50)
+        self.ingredientArea3 = pygame.Rect(1130, 262, 120, 50)
+        self.ingredientArea4 = pygame.Rect(1130, 313, 120, 50)
+        self.ingredientArea5 = pygame.Rect(1130, 364, 120, 50)
+        self.ingredientArea6 = pygame.Rect(1130, 415, 120, 50)
+        self.ingredientArea7 = pygame.Rect(1130, 466, 120, 50)
 
-        self.ingredientArea1 = pygame.Rect(840, 160, 92, 42)
-        self.ingredientArea2 = pygame.Rect(840, 200, 92, 42)
-        self.ingredientArea3 = pygame.Rect(840, 240, 92, 42)
-        self.ingredientArea4 = pygame.Rect(840, 280, 92, 42)
-        self.ingredientArea5 = pygame.Rect(840, 320, 92, 42)
-        self.ingredientArea6 = pygame.Rect(840, 360, 92, 42)
-        self.ingredientArea7 = pygame.Rect(840, 400, 92, 42)
+        self.multiplierArea1 = pygame.Rect(1270, 160, 55, 50)
+        self.multiplierArea2 = pygame.Rect(1270, 211, 55, 50)
+        self.multiplierArea3 = pygame.Rect(1270, 262, 55, 50)
+        self.multiplierArea4 = pygame.Rect(1270, 313, 55, 50)
+        self.multiplierArea5 = pygame.Rect(1270, 364, 55, 50)
+        self.multiplierArea6 = pygame.Rect(1270, 415, 55, 50)
+        self.multiplierArea7 = pygame.Rect(1270, 466, 55, 50)
 
-        self.sliceArea = pygame.Rect(901, 443, 60, 60)
+        self.sliceArea = pygame.Rect(1230, 525, 85, 83)
 
-        # pygame.draw.rect(self.screen, (255, 255, 255), pygame.Rect(self.ingredientArea1), 10)
+        # pygame.draw.rect(self.screen, (255, 255, 255), pygame.Rect(self.sliceArea), 10)
 
     def update(self):
         c = Constants()
@@ -98,7 +98,7 @@ class orderOptions:
 
     def toppingArea(self, x, y, click, color):
         self.TA = pygame.image.load(os.path.join("pictures", "toppingAreaImage.png"))
-        self.TA = pygame.transform.scale(self.TA, (35, 35))
+        self.TA = pygame.transform.scale(self.TA, (43, 43))
 
         if color == 1: # pink
             self.TAbg = pygame.image.load(os.path.join("pictures", "blankToppingAreaPink.png"))
@@ -106,74 +106,76 @@ class orderOptions:
         else: # white
             self.TAbg = pygame.image.load(os.path.join("pictures", "blankToppingAreaWhite.png"))
 
+        self.TAbg = pygame.transform.scale(self.TAbg, (60, 47))
+
         if (click == 1):
-            self.screen.blit(self.TAbg, (x+2, y+3))
-            self.screen.blit(self.TA, (x+16, y+16))
+            self.screen.blit(self.TAbg, (x+2, y+7))
+            self.screen.blit(self.TA, (x+20, y+19))
 
         elif (click == 2):
             self.TA = pygame.transform.flip(self.TA, True, False)
-            self.screen.blit(self.TAbg, (x+2, y+3))
-            self.screen.blit(self.TA, (x-2, y+16))
+            self.screen.blit(self.TAbg, (x+2, y+7))
+            self.screen.blit(self.TA, (x-2, y+19))
 
         elif (click == 3):
             self.TA = pygame.transform.flip(self.TA, True, True)
-            self.screen.blit(self.TAbg, (x+2, y+3))
-            self.screen.blit(self.TA, (x-2, y-4))
+            self.screen.blit(self.TAbg, (x+2, y+7))
+            self.screen.blit(self.TA, (x-2, y-3))
 
         elif (click == 4):
             self.TA = pygame.transform.flip(self.TA, False, True)
-            self.screen.blit(self.TAbg, (x+2, y+3))
-            self.screen.blit(self.TA, (x+16, y-4))
+            self.screen.blit(self.TAbg, (x+2, y+7))
+            self.screen.blit(self.TA, (x+20, y-3))
         
         elif (click == 5):
-            self.screen.blit(self.TAbg, (x+2, y+3))
-            self.screen.blit(self.TA, (x+16, y+16))
+            self.screen.blit(self.TAbg, (x+2, y+7))
+            self.screen.blit(self.TA, (x+20, y+19))
             self.TA = pygame.transform.flip(self.TA, False, True)
-            self.screen.blit(self.TA, (x+16, y-4))
+            self.screen.blit(self.TA, (x+20, y-3))
 
         elif (click == 6):
-            self.screen.blit(self.TAbg, (x+2, y+3))
+            self.screen.blit(self.TAbg, (x+2, y+7))
             self.TA = pygame.transform.flip(self.TA, True, False)
-            self.screen.blit(self.TA, (x-2, y+16))
+            self.screen.blit(self.TA, (x-2, y+19))
             self.TA = pygame.transform.flip(self.TA, False, True)
-            self.screen.blit(self.TA, (x-2, y-4))
+            self.screen.blit(self.TA, (x-2, y-3))
 
         elif (click == 0):
-            self.screen.blit(self.TAbg, (x+2, y+3))
-            self.screen.blit(self.TA, (x+16, y+16))
+            self.screen.blit(self.TAbg, (x+2, y+7))
+            self.screen.blit(self.TA, (x+20, y+19))
             self.TA = pygame.transform.flip(self.TA, False, True)
-            self.screen.blit(self.TA, (x+16, y-4))
+            self.screen.blit(self.TA, (x+20, y-3))
             self.TA = pygame.transform.flip(self.TA, True, True)
-            self.screen.blit(self.TA, (x-2, y+16))
+            self.screen.blit(self.TA, (x-2, y+19))
             self.TA = pygame.transform.flip(self.TA, False, True)
-            self.screen.blit(self.TA, (x-2, y-4))
+            self.screen.blit(self.TA, (x-2, y-3))
 
         self.update()
 
     def ingredientArea(self, x, y, click, color):
         self.artichoke = pygame.image.load(os.path.join("pictures\single_ingredients", "artichoke.png"))
-        self.artichoke = pygame.transform.scale(self.artichoke, (40, 40))
+        self.artichoke = pygame.transform.scale(self.artichoke, (50, 50))
 
         self.mushroom = pygame.image.load(os.path.join("pictures\single_ingredients", "mushroom.png"))
-        self.mushroom = pygame.transform.scale(self.mushroom, (40, 40))
+        self.mushroom = pygame.transform.scale(self.mushroom, (50, 50))
 
         self.olive = pygame.image.load(os.path.join("pictures\single_ingredients", "olive.png"))
-        self.olive = pygame.transform.scale(self.olive, (40, 40))
+        self.olive = pygame.transform.scale(self.olive, (50, 50))
 
         self.onions = pygame.image.load(os.path.join("pictures\single_ingredients", "onions.png"))
-        self.onions = pygame.transform.scale(self.onions, (40, 40))
+        self.onions = pygame.transform.scale(self.onions, (50, 50))
 
         self.pepperoni = pygame.image.load(os.path.join("pictures\single_ingredients", "pepperoni.png"))
-        self.pepperoni = pygame.transform.scale(self.pepperoni, (40, 40))
+        self.pepperoni = pygame.transform.scale(self.pepperoni, (50, 50))
 
         self.pineapple = pygame.image.load(os.path.join("pictures\single_ingredients", "pineapple.png"))
-        self.pineapple = pygame.transform.scale(self.pineapple, (40, 40))
+        self.pineapple = pygame.transform.scale(self.pineapple, (50, 50))
 
         self.spinach = pygame.image.load(os.path.join("pictures\single_ingredients", "spinach.png"))
-        self.spinach = pygame.transform.scale(self.spinach, (40, 40))
+        self.spinach = pygame.transform.scale(self.spinach, (50, 50))
 
         self.tomato = pygame.image.load(os.path.join("pictures\single_ingredients", "tomato.png"))
-        self.tomato = pygame.transform.scale(self.tomato, (40, 40))
+        self.tomato = pygame.transform.scale(self.tomato, (50, 50))
 
         if color == 1: # pink
             self.bgColor = (253, 210, 206)
@@ -182,51 +184,51 @@ class orderOptions:
             self.bgColor = (255, 255, 255)
 
         if (click == 1):
-            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y+2, 38, 38))
+            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y+2, 50, 47))
             self.screen.blit(self.artichoke, (x, y))
 
         elif (click == 2):
-            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y+2, 38, 38))
+            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y+2, 50, 47))
             self.screen.blit(self.mushroom, (x, y))
 
         elif (click == 3):
-            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y+2, 38, 38))
+            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y+2, 50, 47))
             self.screen.blit(self.olive, (x, y))
 
         elif (click == 4):
-            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y+2, 38, 38))
+            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y+2, 50, 47))
             self.screen.blit(self.onions, (x, y))
 
         elif (click == 5):
-            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y+2, 38, 38))
+            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y+2, 50, 47))
             self.screen.blit(self.pepperoni, (x, y))
 
         elif (click == 6):
-            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y+2, 38, 38))
+            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y+2, 50, 47))
             self.screen.blit(self.pineapple, (x, y))
 
         elif (click == 7):
-            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y+2, 38, 38))
+            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y+2, 50, 47))
             self.screen.blit(self.spinach, (x, y))
 
         elif (click == 0):
-            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y+2, 38, 38))
+            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y+2, 50, 42))
             self.screen.blit(self.tomato, (x, y))
 
         self.update()
 
     def multiplierArea(self, x, y, click, color):
         self.M1X = pygame.image.load(os.path.join("pictures", "1x_image.png"))
-        self.M1X = pygame.transform.scale(self.M1X, (40, 40))
+        self.M1X = pygame.transform.scale(self.M1X, (55, 50))
 
         self.M2X = pygame.image.load(os.path.join("pictures", "2x_image.png"))
-        self.M2X = pygame.transform.scale(self.M2X, (40, 40))
+        self.M2X = pygame.transform.scale(self.M2X, (55, 50))
 
         self.M3X = pygame.image.load(os.path.join("pictures", "3x_image.png"))
-        self.M3X = pygame.transform.scale(self.M3X, (40, 40))
+        self.M3X = pygame.transform.scale(self.M3X, (55, 50))
 
         self.M4X = pygame.image.load(os.path.join("pictures", "4x_image.png"))
-        self.M4X = pygame.transform.scale(self.M4X, (40, 40))
+        self.M4X = pygame.transform.scale(self.M4X, (55, 50))
 
         if color == 1: # pink
             self.bgColor = (253, 210, 206)
@@ -235,19 +237,19 @@ class orderOptions:
             self.bgColor = (255, 255, 255)
 
         if (click == 1):
-            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y, 35, 35))
+            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y,55, 50))
             self.screen.blit(self.M1X, (x, y))
 
         elif (click == 2):
-            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y, 35, 35))
+            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y, 55, 50))
             self.screen.blit(self.M2X, (x, y))
 
         elif (click == 3):
-            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y, 35, 35))
+            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y, 55, 50))
             self.screen.blit(self.M3X, (x, y))
 
         elif (click == 0):
-            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y, 35, 35))
+            pygame.draw.rect(self.screen, self.bgColor, pygame.Rect(x, y, 55, 50))
             self.screen.blit(self.M4X, (x, y))
 
         self.update()
